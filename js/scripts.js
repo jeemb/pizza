3c5fa9348d2f825cb8f810f5e9b345930f071499
jb <jenniferbeem@gmail.com>
2017-01-26 20:51:59 -0800

    add pizza constructor and cost method

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
+// business logic
+function Pizza(tppings, size) {
+  this.toppings = toppings;
+  this.size = size;
+}
 
+Pizza.prototype.cost = function() {
+  return 2 * this.toppings + 5 * this.size
+}
